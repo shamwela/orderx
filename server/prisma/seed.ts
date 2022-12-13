ea58a108aaa52909c5efac38170ca47ca0ef22e8
Sha Mwe La <shamwela2002@gmail.com>
2022-12-13 14:26:21 +0630

    Fix prisma seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -2,9 +2,11 @@ import { prisma } from './prismaClient'
 import { restaurants, users, products } from './seedData'
 
 async function main() {
-  await prisma.restaurant.deleteMany()
-  await prisma.user.deleteMany()
+  await prisma.transactionsOnProducts.deleteMany()
+  await prisma.transaction.deleteMany()
   await prisma.product.deleteMany()
+  await prisma.user.deleteMany()
+  await prisma.restaurant.deleteMany()
 
   await prisma.restaurant.createMany({ data: restaurants })
   await prisma.user.createMany({ data: users })
