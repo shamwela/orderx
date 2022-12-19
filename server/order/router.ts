9ff6bc701c096944b28b8877cfd3564993888335
Sha Mwe La <shamwela2002@gmail.com>
2022-12-19 17:24:54 +0630

    Improve security

--- server/order/router.ts
+++ server/order/router.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
+import { rejectUnauthenticatedRequests } from '../middlewares/rejectUnauthenticatedRequests'
 import { createOrder } from './create'
 import { readAllOrders } from './readAll'
 
 export const orderRouter = Router()
-orderRouter.post('/create',  createOrder)
+orderRouter.post('/create', rejectUnauthenticatedRequests, createOrder)
 orderRouter.get('/read-all', readAllOrders)
