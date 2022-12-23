e73578647961fbe7e5dc3d0243d29ea74e307b5d
Sha Mwe La <shamwela2002@gmail.com>
2022-12-23 13:57:06 +0630

    Fix

--- server/utilities/cookieOptions.ts
+++ server/utilities/cookieOptions.ts
@@ -4,6 +4,5 @@ import { clientDomain } from './clientDomain'
 export const cookieOptions: CookieOptions = {
   domain: clientDomain,
   maxAge: 15778800000, // 6 months in milliseconds
-  sameSite: 'none',
   secure: true,
 }
