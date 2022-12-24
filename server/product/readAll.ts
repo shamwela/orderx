550d76a5db7a4c10c1a21c09dcd780c055d3ec35
Sha Mwe La <shamwela2002@gmail.com>
2022-12-24 14:39:11 +0630

    Show only related users products from restaurant

--- server/product/readAll.ts
+++ server/product/readAll.ts
@@ -1,7 +1,9 @@
+import { getJwtUserPayload } from './../utilities/getJwtUserPayload'
 import type { Handler } from 'express'
 import { prisma } from '../prisma/prismaClient'
 
 export const readAllProducts: Handler = async (request, response) => {
-  const products = await prisma.product.findMany()
+  const { restaurantId } = getJwtUserPayload(request)
+  const products = await prisma.product.findMany({ where: { restaurantId } })
   return response.status(200).json(products)
 }
--- server/user/readAll.ts
+++ server/user/readAll.ts
@@ -1,7 +1,9 @@
+import { getJwtUserPayload } from './../utilities/getJwtUserPayload'
 import type { Handler } from 'express'
 import { prisma } from '../prisma/prismaClient'
 
 export const readAllUsers: Handler = async (request, response) => {
-  const users = await prisma.user.findMany()
+  const { restaurantId } = getJwtUserPayload(request)
+  const users = await prisma.user.findMany({ where: { restaurantId } })
   return response.json(users)
 }
